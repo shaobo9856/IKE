9c602ece9f8f1e00b47b67da5317c46c80c62c87
shaobo9856 <26827545+shaobo9856@users.noreply.github.com>
2024-06-07 15:28:24 +0200

    add scripts for batch run

---
+++ run_icls.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python icl.py --lang1 en --lang2 af --tdata MzsRE/mzsre_test_duplicate_ --pdata zsre_multi
+python icl.py --lang1 en --lang2 ar --tdata MzsRE/mzsre_test_duplicate_ --pdata zsre_multi
+python icl.py --lang1 en --lang2 az --tdata MzsRE/mzsre_test_duplicate_ --pdata zsre_multi
