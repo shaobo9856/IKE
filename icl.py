5993fa5314fd2fa9b658631bb2963e09b065b539
shaobo9856 <26827545+shaobo9856@users.noreply.github.com>
2024-06-07 16:04:00 +0200

    de

--- icl.py
+++ icl.py
@@ -159,58 +159,58 @@ if __name__ == '__main__':
 
         icl_examples.append(f'New Fact: {prompts_truth} {target_truth}\nPrompt: {prompts_test}{target_test}\n\n')  # 要不要加prompts_test + target_test。  Prompt: {prompts_test}{target_test}\n\n
 
-        # if "MzsRE" in args.tdata:
+        if "MzsRE" in args.tdata:
             # reliablilty (f1em)
-        ans = icl_lm_eval_f1em(model,tokenizer, icl_examples, target_test, f'New Fact: {prompts_truth} {target_truth}\nPrompt: {prompts_test}')
-        # print(f"ans:{ans}, target: {target_test}")
-        wrap_f1em_list(reliablilty_f1_list, reliablilty_em_list, ans, target_test)
+            ans = icl_lm_eval_f1em(model,tokenizer, icl_examples, target_test, f'New Fact: {prompts_truth} {target_truth}\nPrompt: {prompts_test}')
+            # print(f"ans:{ans}, target: {target_test}")
+            wrap_f1em_list(reliablilty_f1_list, reliablilty_em_list, ans, target_test)
 
-        # generalization (f1em)
-        ans = icl_lm_eval_f1em(model,tokenizer, icl_examples, target_test, f'New Fact: {prompts_truth} {target_truth}\nPrompt: {rephrase_prompt}')
-        wrap_f1em_list(generalization_f1_list, generalization_em_list, ans, target_test)
+            # generalization (f1em)
+            ans = icl_lm_eval_f1em(model,tokenizer, icl_examples, target_test, f'New Fact: {prompts_truth} {target_truth}\nPrompt: {rephrase_prompt}')
+            wrap_f1em_list(generalization_f1_list, generalization_em_list, ans, target_test)
 
-        # locality (f1em)
-        ans = icl_lm_eval_f1em(model,tokenizer, icl_examples, locality_an, locality_prompt)
-        wrap_f1em_list(locality_f1_list, locality_em_list, ans, target_test)
+            # locality (f1em)
+            ans = icl_lm_eval_f1em(model,tokenizer, icl_examples, locality_an, locality_prompt)
+            wrap_f1em_list(locality_f1_list, locality_em_list, ans, target_test)
 
-        # portablility (f1em)
-        ans = icl_lm_eval_f1em(model,tokenizer, icl_examples, portability_an, portability_prompt)
-        wrap_f1em_list(portablility_f1_list, portablility_em_list, ans, target_test)
-        # elif  "MCounter" in args.tdata:
-        #     print("22")
-        #     # reliablilty (ppls)
-        #     edit_ppls = icl_lm_eval_ppls(model,tokenizer, icl_examples, [target_test, line[args.lang2]['loc_ans']], f'New Fact: {prompts_truth} {target_truth}\nPrompt: {prompts_test}')
-        #     wrap_ppls_count(edit_ppls, orig_total_cnt, orig_success_cnt, orig_magnitude)
+            # portablility (f1em)
+            ans = icl_lm_eval_f1em(model,tokenizer, icl_examples, portability_an, portability_prompt)
+            wrap_f1em_list(portablility_f1_list, portablility_em_list, ans, target_test)
+        elif  "MCounter" in args.tdata:
+            print("22")
+            # reliablilty (ppls)
+            edit_ppls = icl_lm_eval_ppls(model,tokenizer, icl_examples, [target_test, line[args.lang2]['loc_ans']], f'New Fact: {prompts_truth} {target_truth}\nPrompt: {prompts_test}')
+            wrap_ppls_count(edit_ppls, orig_total_cnt, orig_success_cnt, orig_magnitude)
 
-        #     # generalization (ppls)
-        #     edit_ppls = icl_lm_eval_ppls(model,tokenizer, icl_examples, [target_test, line[args.lang2]['loc_ans']], f'New Fact: {prompts_truth} {target_truth}\nPrompt: {rephrase_prompt}')
-        #     wrap_ppls_count(edit_ppls, para_total_cnt, para_success_cnt, para_magnitude)
+            # generalization (ppls)
+            edit_ppls = icl_lm_eval_ppls(model,tokenizer, icl_examples, [target_test, line[args.lang2]['loc_ans']], f'New Fact: {prompts_truth} {target_truth}\nPrompt: {rephrase_prompt}')
+            wrap_ppls_count(edit_ppls, para_total_cnt, para_success_cnt, para_magnitude)
 
-        #     # locality (ppls)
-        #     edit_ppls = icl_lm_eval_ppls(model,tokenizer, icl_examples, [locality_an, line[args.lang2]['loc_ans']], locality_prompt)
-        #     wrap_ppls_count(edit_ppls, total_cnt, success_cnt, magnitude)
+            # locality (ppls)
+            edit_ppls = icl_lm_eval_ppls(model,tokenizer, icl_examples, [locality_an, line[args.lang2]['loc_ans']], locality_prompt)
+            wrap_ppls_count(edit_ppls, total_cnt, success_cnt, magnitude)
 
-        #     # portablility (f1em)
-        #     ans = icl_lm_eval_f1em(model,tokenizer, icl_examples, portability_an, portability_prompt)
-        #     wrap_f1em_list(portablility_f1_list, portablility_em_list, ans, target_test)
-        # elif  "WikiFact" in args.tdata:
-        #     # reliablilty (ppls)
-        #     edit_ppls = icl_lm_eval_ppls(model,tokenizer, icl_examples, [target_test, line[args.lang2]['loc_ans']], f'New Fact: {prompts_truth} {target_truth}\nPrompt: {prompts_test}')
-        #     wrap_ppls_count(edit_ppls, orig_total_cnt, orig_success_cnt, orig_magnitude)
+            # portablility (f1em)
+            ans = icl_lm_eval_f1em(model,tokenizer, icl_examples, portability_an, portability_prompt)
+            wrap_f1em_list(portablility_f1_list, portablility_em_list, ans, target_test)
+        elif  "WikiFact" in args.tdata:
+            # reliablilty (ppls)
+            edit_ppls = icl_lm_eval_ppls(model,tokenizer, icl_examples, [target_test, line[args.lang2]['loc_ans']], f'New Fact: {prompts_truth} {target_truth}\nPrompt: {prompts_test}')
+            wrap_ppls_count(edit_ppls, orig_total_cnt, orig_success_cnt, orig_magnitude)
 
-        #     # generalization (ppls)
-        #     edit_ppls = icl_lm_eval_ppls(model,tokenizer, icl_examples, [target_test, line[args.lang2]['loc_ans']], f'New Fact: {prompts_truth} {target_truth}\nPrompt: {rephrase_prompt}')
-        #     wrap_ppls_count(edit_ppls, para_total_cnt, para_success_cnt, para_magnitude)
+            # generalization (ppls)
+            edit_ppls = icl_lm_eval_ppls(model,tokenizer, icl_examples, [target_test, line[args.lang2]['loc_ans']], f'New Fact: {prompts_truth} {target_truth}\nPrompt: {rephrase_prompt}')
+            wrap_ppls_count(edit_ppls, para_total_cnt, para_success_cnt, para_magnitude)
 
-        #     # locality (f1em)
-        #     ans = icl_lm_eval_f1em(model,tokenizer, icl_examples, locality_an, locality_prompt)
-        #     wrap_f1em_list(locality_f1_list, locality_em_list, ans, target_test)
+            # locality (f1em)
+            ans = icl_lm_eval_f1em(model,tokenizer, icl_examples, locality_an, locality_prompt)
+            wrap_f1em_list(locality_f1_list, locality_em_list, ans, target_test)
 
-        #     # portablility (f1em)
-        #     ans = icl_lm_eval_f1em(model,tokenizer, icl_examples, portability_an, portability_prompt)
-        #     wrap_f1em_list(portablility_f1_list, portablility_em_list, ans, target_test)
-        # else:
-        #     print("unvalid test data")
+            # portablility (f1em)
+            ans = icl_lm_eval_f1em(model,tokenizer, icl_examples, portability_an, portability_prompt)
+            wrap_f1em_list(portablility_f1_list, portablility_em_list, ans, target_test)
+        else:
+            print("unvalid test data")
 
         example_idx += 1
         print(example_idx)
