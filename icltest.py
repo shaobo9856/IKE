ebd1704a270878a69c1111dcf65257946baee262
shaobo9856 <26827545+shaobo9856@users.noreply.github.com>
2024-06-07 16:49:04 +0200

    Update icltest.py

--- icltest.py
+++ icltest.py
@@ -83,7 +83,7 @@ def construct_icl_examples():
     with open(f'./data/manual_prompts/{args.pdata}.json', 'r') as fIn: # mcounterfact_multi   zsre_multi   wfd_multi
         lines = json.load(fIn)
         for line in lines:
-            print(line[:5])
+            print(line['new_fact'])
         lang1 = line['new_fact'] if args.lang1 == 'en' else args.lang1
         icl_examples.append(f"New Fact: {lang1} \nPrompt: {line[args.lang2]} \n\n")
     icl_examples.reverse()
