95c70f2465ffc25abdb57edac1047b226c13500d
shaobo9856 <26827545+shaobo9856@users.noreply.github.com>
2024-06-08 23:40:17 +0200

    Update icl.py

--- icl.py
+++ icl.py
@@ -234,7 +234,7 @@ if __name__ == '__main__':
             para_total_cnt, para_success_cnt, para_magnitude = wrap_ppls_count(edit_ppls, para_total_cnt, para_success_cnt, para_magnitude)
 
             # locality (ppls)
-            edit_ppls = icl_lm_eval_ppls(model,tokenizer, icl_examples_manual, [target_test,locality_an, ], f'New Fact: {prompts_truth} {target_truth}\nPrompt: {locality_prompt}')
+            edit_ppls = icl_lm_eval_ppls(model,tokenizer, icl_examples_manual, [locality_an, target_test, ], f'New Fact: {prompts_truth} {target_truth}\nPrompt: {locality_prompt}')
             total_cnt, success_cnt, magnitude = wrap_ppls_count(edit_ppls, total_cnt, success_cnt, magnitude)
             print(f"prompts_truth {prompts_truth}")
             print(f"target_truth {target_truth}")
