8afbc488b7c715a592d29fc4de15d1d38d1788c8
shaobo9856 <26827545+shaobo9856@users.noreply.github.com>
2024-06-14 15:25:27 +0200

    clean useless run

--- search/run_icls.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python icl.py --lang1 en --lang2 af --tdata MzsRE/mzsre_test_duplicate_ --pdata zsre_multi
-python icl.py --lang1 en --lang2 ar --tdata MzsRE/mzsre_test_duplicate_ --pdata zsre_multi
-python icl.py --lang1 en --lang2 az --tdata MzsRE/mzsre_test_duplicate_ --pdata zsre_multi
